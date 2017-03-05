2a20a24961cb43ef0c390867f2ccdc79f927bfe6
Neil <neilwood1997@gmail.com>
2017-03-05 17:10:52 +0000

    Minor work towards finding pages from cats page

--- gsp/get_set_pet_project/gsp/views.py
+++ gsp/get_set_pet_project/gsp/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from gsp.models import Category
+from gsp.models import Page
+
 
 def index(request):
-	return render(request, 'gsp/index.html', context=context_dict)
+	return render(request, 'gsp/index.html')
 
 def about(request):
 	return HttpResponse('This is the about page  <a href="/gsp/">Index</a>')
@@ -14,3 +16,14 @@ def cats(request):
 	context_dict = {'categories': category_list}
 	return render(request, 'gsp/cats.html', context=context_dict)
 	
+def show_category(request, category_name_slug): 
+	context_dict = {}
+	try:
+		category = Category.objects.get(slug=category_name_slug)
+		pages = Page.objects.filter(category=category)
+		context_dict['pages'] = pages 
+		context_dict['category'] = category 
+	except Category.DoesNotExist: 
+		context_dict['category'] = None 
+		context_dict['pages'] = None
+	return render(request, 'rango/category.html', context_dict)
