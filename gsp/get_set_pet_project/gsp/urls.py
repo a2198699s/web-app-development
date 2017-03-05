9b266da2a9b90717e71b822f68060105c77a9bd3
Neil <neilwood1997@gmail.com>
2017-03-05 15:51:08 +0000

    adding category page

--- gsp/get_set_pet_project/gsp/urls.py
+++ gsp/get_set_pet_project/gsp/urls.py
@@ -3,6 +3,8 @@ from gsp import views
 
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^about/', views.about, name='about')
-]
+	url(r'^$', views.index, name='index'),
+	url(r'^about/', views.about, name='about'),
+	url(r'^cats/', views.cats, name='cats'),
+	url(r'about/$', views.about, name='about')
+	]
--- gsp/get_set_pet_project/gsp/views.py
+++ gsp/get_set_pet_project/gsp/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from gsp.models import Category
 
 def index(request):
-    context_dict = {'boldmessage': "Just checking everything's tickety-boo"}
-    return render(request, 'gsp/index.html', context=context_dict)
+	return render(request, 'gsp/index.html', context=context_dict)
 
 def about(request):
-    return HttpResponse('This is the about page  <a href="/gsp/">Index</a>')
+	return HttpResponse('This is the about page  <a href="/gsp/">Index</a>')
+
+
+def cats(request):
+	category_list = Category.objects.order_by('-likes') 
+	context_dict = {'categories': category_list}
+	return render(request, 'gsp/cats.html', context=context_dict)
+	
