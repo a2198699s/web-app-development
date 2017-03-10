dac77fbeab13b2700e8d83056a8592dc34cf4619
“FiNeil <202197680wtudent.gla.ac.uk>
2017-03-10 16:35:24 +0000

    adding user Auth part2

--- gsp/get_set_pet_project/gsp/urls.py
+++ gsp/get_set_pet_project/gsp/urls.py
@@ -7,7 +7,22 @@ urlpatterns = [
 	url(r'^about/', views.about, name='about'),
 	url(r'^cats/', views.cats, name='cats'),
 	url(r'about/$', views.about, name='about'),
+	url(r'^add_category/$', views.add_category, name='add_category'),
 	url(r'^category/(?P<category_name_slug>[\w\-]+)/$', 
 	views.show_category, 
 	name='show_category'),
+	url(r'^category/(?P<category_name_slug>[\w\-]+)/add_page/$',views.add_page,name='add_page'),
+	url(r'^register/$',views.register,name='register'), 
+
 	]
+	
+#Handy Links	
+	
+#<a href="/gsp/add_category/">Add a New Category</a><br /> Link to add_category
+#<ul>
+#<li><a href="{% url 'add_category' %}">Add a New Category</a></li>
+#<li><a href="{% url 'about' %}">About</a></li>
+#<li><a href="{% url 'index' %}">Index</a></li>
+#<li><a href="{% url 'register' %}">Sign Up</a></li>
+#</ul>
+
--- gsp/get_set_pet_project/gsp/views.py
+++ gsp/get_set_pet_project/gsp/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from gsp.models import Category
 from gsp.models import Page
+from gsp.forms import CategoryForm
+from gsp.forms import PageForm
+from gsp.forms import UserForm, UserProfileForm
 
 
 def index(request):
@@ -27,3 +30,65 @@ def show_category(request, category_name_slug):
 		context_dict['category'] = None 
 		context_dict['pages'] = None
 	return render(request, 'gsp/category.html', context_dict)
+	
+	
+def add_category(request):
+	form = CategoryForm()
+	if request.method == 'POST':
+		form = CategoryForm(request.POST)
+		if form.is_valid():
+			form.save(commit=True)
+			return index(request)
+		else:
+			print(form.errors)
+	return render(request, 'gsp/add_category.html', {'form': form})
+	
+
+def add_page(request, category_name_slug):
+	try:
+		category = Category.objects.get(slug=category_name_slug)
+	except Category.DoesNotExist:
+		category = None
+	form = PageForm()
+	if request.method == 'POST':
+		form = PageForm(request.POST)
+		if form.is_valid():
+			if category:
+				page = form.save(commit=False)
+				page.category = category
+				page.views = 0
+				page.save()
+				return show_category(request, category_name_slug)
+		else:
+			print(form.errors)
+	context_dict = {'form':form, 'category': category}
+	return render(request, 'gsp/add_page.html', context_dict)
+	
+def register(request):
+	registered = False
+	if request.method == 'POST':
+		user_form = UserForm(data=request.POST)
+		profile_form = UserProfileForm(data=request.POST)
+		if user_form.is_valid() and profile_form.is_valid():
+			user = user_form.save()
+			user.set_password(user.password)
+			user.save()
+			profile = profile_form.save(commit=False)
+			profile.user = user
+			if 'picture' in request.FILES:
+				profile.picture = request.FILES['picture']
+			profile.save()
+			registered = True
+		else:
+			print(user_form.errors, profile_form.errors)
+	else:
+		user_form = UserForm()
+		profile_form = UserProfileForm()
+	return render(request,
+		'gsp/register.html',
+		{'user_form': user_form,
+		'profile_form': profile_form,
+		'registered': registered})
+
+
+
